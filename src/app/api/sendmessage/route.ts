44c69b87fc6de4a876d972d458d98a5a19d0a8bc
Yash Chopra <chopray567@gmail.com>
2024-03-21 23:01:59 +0530

    commit

--- src/app/api/sendmessage/route.ts
+++ src/app/api/sendmessage/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
     try {
         const data = await req.json();
-        console.log(data)
+
         const emailResponse = await SendMail(data.email, data.message, data.name);
         return NextResponse.json({
             success: true,
