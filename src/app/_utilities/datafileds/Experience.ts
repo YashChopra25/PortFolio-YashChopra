e970f3216eff8e7371167404fee6e4d5525d8f26
Yash Chopra <99895398+YashChopra25@users.noreply.github.com>
2024-11-16 18:45:39 +0530

    Update Experience.ts
    update experience

--- src/app/_utilities/datafileds/Experience.ts
+++ src/app/_utilities/datafileds/Experience.ts
@@ -1,24 +1,69 @@
 type ExperienceType = {
-    position: string,
-    company: string,
-    keypoint: string[]
-    skills: string[]
-}
+  position: string;
+  company: string;
+  keypoint: string[];
+  skills: string[];
+};
 export const ExperienceData: ExperienceType[] = [
-    {
-        position: "Intern - Full Stack Developer",
-        company: "qunit technologies pvt. ltd | Remote | Jan 2024 | July 2024",
-        skills: ['nextjs,', 'nextAuth,', 'AWS,', 'MONGODB'],
-        keypoint: [
-            " Designed and developed responsive user interfaces using Next.js framework and Tailwind CSS, ensuring a seamless and visually appealing user experience for a new online learning and teaching platform.",
-
-            "Implemented serverless backend logic within Next.js API routes, utilizing AWS Lambda functions for dynamic data processing and integrating with MongoDB Atlas for database storage, contributing to the platform's scalability and performance optimization.",
-
-            "Integrated user authentication and authorization features using NextAuth.js within the Next.js application, providing secure login/ logout functionalities and managing user sessions effectively.",
-
-            "Conducted unit testing using Jest for frontend components and integration testing for Next.js API routes, ensuring code reliability and functionality across the application.",
-
-            "Actively participated in Agile scrum meetings, code reviews, and version control with Git, gaining valuable teamwork and collaboration skills in a professional development environment.",
-        ]
-    },
-]
+  {
+    position: "Software Associate Engineer - MERN Developer",
+    company: "Black Coffer | Remote | Aug 2024 | Present",
+    keypoint: [
+      "Developed an seamless Ui for the client by using React.js along with the react-flow and react-flow-renderer library.",
+      "Implemented a user authentication system using NextAuth.js for authentication and authorization.",
+      "Developed a dynamic form builder using React.js and Tailwind CSS.",
+      "Developed a dynamic diagram builder using React.js and Tailwind CSS. like sequence diagram ,class diagram,Er diagram and many others",
+    ],
+    skills: [
+      "React",
+      "Express",
+      "Node",
+      "GCP",
+      "EC2",
+      "MONGODB",
+      "ELASTIC SEARCH",
+      "AWS",
+      "NEO4J",
+      "DOCKER",
+      "JENKINS",
+      "GITHUB",
+      "GITLAB",
+      "JIRA",
+    ],
+  },
+  {
+    position: "Intern - Full Stack Developer",
+    company: "qunit technologies pvt. ltd | Remote | Jan 2024 | July 2024",
+    skills: ["nextjs,", "nextAuth,", "AWS,", "MONGODB"],
+    keypoint: [
+      " Designed and developed responsive user interfaces using Next.js framework and Tailwind CSS, ensuring a seamless and visually appealing user experience for a new online learning and teaching platform.",
+      "Implemented serverless backend logic within Next.js API routes, utilizing AWS Lambda functions for dynamic data processing and integrating with MongoDB Atlas for database storage, contributing to the platform's scalability and performance optimization.",
+      "Integrated user authentication and authorization features using NextAuth.js within the Next.js application, providing secure login/ logout functionalities and managing user sessions effectively.",
+      "Conducted unit testing using Jest for frontend components and integration testing for Next.js API routes, ensuring code reliability and functionality across the application.",
+      "Actively participated in Agile scrum meetings, code reviews, and version control with Git, gaining valuable teamwork and collaboration skills in a professional development environment.",
+    ],
+  },
+  {
+    position: "MERN Developer",
+    company: "Dokopi | Remote | May 2024 | August 2024",
+    keypoint: [
+      "Developed and launched a marketplace for xerox stores, Dokopi, to serve the college campus community Implemented separate panels for merchants partners, admins, and users to streamline operations and enhance user experience",
+      "Implemented a user authentication system using NextAuth.js for authentication and authorization.",
+      "Used Next.js for frontend and Express.js for backend development.",
+      "Integrated agile payment price method for merchant and customer,calculate price at distinct quantity",
+    ],
+    skills: [
+      "React",
+      "Express",
+      "Node",
+      "EC2",
+      "MONGODB",
+      "AWS",
+      "NEO4J",
+      "DOCKER",
+      "GITHUB Actions",
+      "GITHUB",
+      "JIRA",
+    ],
+  },
+];
